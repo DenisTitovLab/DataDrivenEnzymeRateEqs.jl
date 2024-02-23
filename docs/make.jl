9e0f0c276a1283d2e7983275cee77f20b209769f
Denis-Titov <titov@berkeley.edu>
2024-02-23 15:46:40 -0800

    some edits to buuld docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,17 +7,19 @@ makedocs(;
     modules=[EnzymeFitting],
     authors="Denis Titov titov@berkeley.edu and contributors",
     sitename="EnzymeFitting.jl",
-    format=Documenter.HTML(;
-        canonical="https://Denis-Titov.github.io/EnzymeFitting.jl",
-        edit_link="main",
-        assets=String[],
-    ),
+    # remotes = Documenter.Remotes.GitHub("DenisTitovLab", "EnzymeFitting.jl"),
+    repo = Documenter.Remotes.GitHub("DenisTitovLab", "EnzymeFitting.jl"),
+    # format=Documenter.HTML(;
+    #     canonical="https://Denis-Titov.github.io/EnzymeFitting.jl",
+    #     edit_link="main",
+    #     assets=String[],
+    # ),
     pages=[
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/Denis-Titov/EnzymeFitting.jl",
+    repo="github.com/DenisTitovLab/EnzymeFitting.jl.git",
     devbranch="main",
 )
