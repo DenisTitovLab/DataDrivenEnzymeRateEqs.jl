0a8249f7bcf0d963d7608722d31d9dea88026c4a
Denis-Titov <titov@berkeley.edu>
2024-02-23 12:33:18 -0800

    remove @time label to make compatible with julia 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EnzymeFitting
 using Test
 using SafeTestsets
 
-@time "Total test time" begin
+@time begin
     @time @safetestset "MWC Rate Eq Derivation" begin
         include("tests_for_rate_eq_derivation.jl")
     end
