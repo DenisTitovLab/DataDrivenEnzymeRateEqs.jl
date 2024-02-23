e4c2c3addcdcb4d88480302e626797caa6650f0f
Denis-Titov <titov@berkeley.edu>
2024-02-22 18:25:09 -0800

    edit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EnzymeFitting
 using Test
 using SafeTestsets
 
-@time begin
+@time "Total test time" begin
     @time @safetestset "MWC Rate Eq Derivation" begin
         include("tests_for_rate_eq_derivation.jl")
     end
