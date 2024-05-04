783ecbb32f5d2a1f77b31f1e20f1886241917d30
Denis-Titov <titov@berkeley.edu>
2024-05-03 22:59:52 -0700

    more edits to rapid equilibrium code

--- test/tests_for_rapid_equilibrium_general_rate_eq_derivation.jl
+++ test/tests_for_rapid_equilibrium_general_rate_eq_derivation.jl
@@ -20,30 +20,30 @@ benchmark_result = @benchmark rand_enz_rate_equation(metabs_nt, params_nt, 20000
 @test mean(benchmark_result.times) <= 150 #ns
 @test benchmark_result.allocs <= 1
 
-#test Rate < 0 when [Substrates] = 0
-metabs_nt =
-    (PEP=0.0, ADP=0.0, Pyruvate=1.0e-3, ATP=1.0e-3, F16BP=1.0e-3, Phenylalanine=1.0e-3)
-@test pkm2_rate_equation(metabs_nt, params_nt, 20000.0) < 0.0
+# #test Rate < 0 when [Substrates] = 0
+# metabs_nt =
+#     (PEP=0.0, ADP=0.0, Pyruvate=1.0e-3, ATP=1.0e-3, F16BP=1.0e-3, Phenylalanine=1.0e-3)
+# @test pkm2_rate_equation(metabs_nt, params_nt, 20000.0) < 0.0
 
-#test Rate > 0 when [Products] = 0
-metabs_nt =
-    (PEP=1.0e-3, ADP=1.0e-3, Pyruvate=0.0, ATP=0.0, F16BP=1.0e-3, Phenylalanine=1.0e-3)
-@test pkm2_rate_equation(metabs_nt, params_nt, 20000.0) > 0.0
+# #test Rate > 0 when [Products] = 0
+# metabs_nt =
+#     (PEP=1.0e-3, ADP=1.0e-3, Pyruvate=0.0, ATP=0.0, F16BP=1.0e-3, Phenylalanine=1.0e-3)
+# @test pkm2_rate_equation(metabs_nt, params_nt, 20000.0) > 0.0
 
-#test Rate = Vmax_a/i when [Substrates] -> Inf and Vmax_a/i_rev when [Products] -> Inf
-Vmax_a = 1.0
-Vmax_i = 1.0
-metabs_nt =
-    (PEP=1e12, ADP=1e12, Pyruvate=0.0, ATP=0.0, F16BP=1.0e12, Phenylalanine=0.0)
-@test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), Vmax_a, rtol=1e-6)
-metabs_nt =
-    (PEP=1e12, ADP=1e12, Pyruvate=0.0, ATP=0.0, F16BP=0.0, Phenylalanine=1.0e12)
-@test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), Vmax_i, rtol=1e-6)
-Vmax_a_rev = params_nt.Vmax_a * params_nt.K_a_ATP_cat1 * params_nt.K_a_Pyruvate_cat2 / (20000.0 * params_nt.K_a_PEP_cat2 * params_nt.K_a_ADP_cat1)
-Vmax_i_rev = params_nt.Vmax_i * params_nt.K_i_ATP_cat1 * params_nt.K_i_Pyruvate_cat2 / (20000.0 * params_nt.K_i_PEP_cat2 * params_nt.K_i_ADP_cat1)
-metabs_nt =
-    (PEP=0.0, ADP=0.0, Pyruvate=1e24, ATP=1e24, F16BP=1.0e12, Phenylalanine=0.0)
-@test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), -Vmax_a_rev, rtol=1e-6)
-metabs_nt =
-    (PEP=0.0, ADP=0.0, Pyruvate=1e12, ATP=1e12, F16BP=0.0, Phenylalanine=1e12)
-@test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), -Vmax_i_rev, rtol=1e-6)
+# #test Rate = Vmax_a/i when [Substrates] -> Inf and Vmax_a/i_rev when [Products] -> Inf
+# Vmax_a = 1.0
+# Vmax_i = 1.0
+# metabs_nt =
+#     (PEP=1e12, ADP=1e12, Pyruvate=0.0, ATP=0.0, F16BP=1.0e12, Phenylalanine=0.0)
+# @test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), Vmax_a, rtol=1e-6)
+# metabs_nt =
+#     (PEP=1e12, ADP=1e12, Pyruvate=0.0, ATP=0.0, F16BP=0.0, Phenylalanine=1.0e12)
+# @test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), Vmax_i, rtol=1e-6)
+# Vmax_a_rev = params_nt.Vmax_a * params_nt.K_a_ATP_cat1 * params_nt.K_a_Pyruvate_cat2 / (20000.0 * params_nt.K_a_PEP_cat2 * params_nt.K_a_ADP_cat1)
+# Vmax_i_rev = params_nt.Vmax_i * params_nt.K_i_ATP_cat1 * params_nt.K_i_Pyruvate_cat2 / (20000.0 * params_nt.K_i_PEP_cat2 * params_nt.K_i_ADP_cat1)
+# metabs_nt =
+#     (PEP=0.0, ADP=0.0, Pyruvate=1e24, ATP=1e24, F16BP=1.0e12, Phenylalanine=0.0)
+# @test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), -Vmax_a_rev, rtol=1e-6)
+# metabs_nt =
+#     (PEP=0.0, ADP=0.0, Pyruvate=1e12, ATP=1e12, F16BP=0.0, Phenylalanine=1e12)
+# @test isapprox(pkm2_rate_equation(metabs_nt, params_nt, 20000.0), -Vmax_i_rev, rtol=1e-6)
