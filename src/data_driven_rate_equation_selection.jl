322bae313531e2497ce86d3894e9e677c24bc92b
Denis-Titov <titov@berkeley.edu>
2024-05-23 17:12:10 -0700

    add ability to remove qssa K as product of other Ks

--- src/data_driven_rate_equation_selection.jl
+++ src/data_driven_rate_equation_selection.jl
@@ -241,6 +241,11 @@ function calculate_all_parameter_removal_codes(param_names::Tuple{Symbol,Vararg{
                !startswith(string(param_name), "K_i") &&
                !startswith(string(param_name), "K_a")
             feasible_param_subset_codes = (feasible_param_subset_codes..., [0, 1])
+        elseif startswith(string(param_name), "K_") &&
+               !startswith(string(param_name), "K_i") &&
+               !startswith(string(param_name), "K_a") &&
+               length(split(param_choice, "_")) > 3
+            feasible_param_subset_codes = (feasible_param_subset_codes..., [0, 1, 2])
         elseif startswith(string(param_name), "alpha")
             feasible_param_subset_codes = (feasible_param_subset_codes..., [0, 1])
         end
@@ -282,6 +287,14 @@ function param_subset_select(params, param_names, nt_param_removal_code)
                !startswith(string(param_choice), "K_allo") &&
                nt_param_removal_code[param_choice] == 1
             params_dict[param_choice] = Inf
+        elseif startswith(string(param_choice), "K_") &&
+               !startswith(string(param_choice), "K_allo") &&
+               length(split(param_choice, "_")) > 3 &&
+               nt_param_removal_code[param_choice] == 2
+            params_dict[param_choice] = prod([
+                params_dict[Symbol("K_" * string(metab))] for
+                metab in split(param_choice, "_")[3:end]
+            ])
         elseif startswith(string(param_choice), "alpha") &&
                nt_param_removal_code[param_choice] == 0
             params_dict[param_choice] = 0.0
