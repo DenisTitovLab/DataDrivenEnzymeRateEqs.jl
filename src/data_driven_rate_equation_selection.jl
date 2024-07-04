84036577ff7e75ee0fcd85b2184e13b8515d7279
Denis-Titov <titov@berkeley.edu>
2024-07-04 12:14:10 -0700

    modify range_number_params to find feasible starting equations

--- src/data_driven_rate_equation_selection.jl
+++ src/data_driven_rate_equation_selection.jl
@@ -76,11 +76,26 @@ function data_driven_rate_equation_selection(
         metab_names,
         num_alpha_params,
     )
-    if isempty(starting_param_removal_codes)
-        @error "Equations for this enzymes with $(num_param_range[1]) parameters do not exist. One reason could be that some parameters are unidentifiable based on the data so upper bound of range_number_params should be reduced. Check number of practically_unidentifiable_params with find_practically_unidentifiable_params(data, param_names)."
-        return
-    end
 
+    while isempty(starting_param_removal_codes)
+        num_param_range = ifelse(
+            forward_model_selection,
+            (num_param_range[1] - 1, num_param_range[end]),
+            (num_param_range[1] + 1, num_param_range[end])
+        )
+        if num_param_range[1] == num_param_range[end]
+            @error "Could not find any fesible equations for this enzyme within range_number_params"
+        end
+        println("Trying new range_number_params: $num_param_range")
+        starting_param_removal_codes = DataDrivenEnzymeRateEqs.calculate_all_parameter_removal_codes_w_num_params(
+            num_param_range[1],
+            all_param_removal_codes,
+            param_names,
+            param_removal_code_names,
+            metab_names,
+            num_alpha_params,
+        )
+    end
 
     nt_param_removal_codes = starting_param_removal_codes
     nt_previous_param_removal_codes = similar(nt_param_removal_codes)
